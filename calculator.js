262b891c8f60d6b5587ac47b3ceb2f2b4abf0b6e
Frank / <144677258+frankosparks@users.noreply.github.com>
2024-02-14 21:37:39 +0300

    calculator.js
    initial commit

---
+++ calculator.js
@@ -0,0 +1,3 @@
+this is a simple calculator that does basic addition,subtraction,multiplication and division.
+One is allowed to enter the number or arithmetic problem one wants to do.
+It is displayed then after clicking = ,the solution is displayed.
