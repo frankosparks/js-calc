d7cab838fdb14211295987cd088a5f2d5e7aebff
Frank / <144677258+frankosparks@users.noreply.github.com>
2024-05-01 18:56:50 +0300

    Update calculator.js
    second commit

--- calculator.js
+++ calculator.js
@@ -1,3 +1,8 @@
-this is a simple calculator that does basic addition,subtraction,multiplication and division.
-One is allowed to enter the number or arithmetic problem one wants to do.
-It is displayed then after clicking = ,the solution is displayed.
+This is a simple calculator that does basic addition,subtraction,multiplication and division.
+One is allowed to enter the number or arithmetic problem one wants to solve.
+It is displayed then after clicking '=' ,the solution is calculated then displayed.
+It uses lots of logic and use of if...else statements.
+Previous calculation is stored in the local storage using setItem().
+It is easily retrived using getItem().
+Using the arithmetic operators the math is done.
+CSS has styled the buttons and the web page overall.
